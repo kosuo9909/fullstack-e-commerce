d7c40f61e136786f961088a5d0e2fe2c72a9e023
kosuo9909 <stanevbsn@gmail.com>
2025-03-25 17:50:54 +0200

    add search functionality

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -1,4 +1,3 @@
-using System.Security.Cryptography.X509Certificates;
 using Core.Entities;
 
 namespace Core.Specifications;
@@ -6,6 +5,7 @@ namespace Core.Specifications;
 public class ProductSpecification : BaseSpecifications<Product>
 {
     public ProductSpecification(ProductSpecParams specParams) : base(p =>
+    (string.IsNullOrEmpty(specParams.Search) || p.Name.ToLower().Contains(specParams.Search)) &&
         (!specParams.Brands.Any() || specParams.Brands.Contains(p.Brand)) &&
         (!specParams.Types.Any() || specParams.Types.Contains(p.Type))
     )
