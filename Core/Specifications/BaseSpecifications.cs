d145aaef9d882d8baf3a01d4db1feb93fcc6cb07
kosuo9909 <stanevbsn@gmail.com>
2025-03-25 11:15:23 +0200

    adding projections

---
+++ Core/Specifications/BaseSpecifications.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq.Expressions;
+using Core.Interfaces;
+
+namespace Core.Specifications;
+
+public class BaseSpecifications<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
+
+{
+    protected BaseSpecifications() : this(null) { }
+    public Expression<Func<T, bool>>? Criteria => criteria;
+
+    public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+    public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+    protected void AddOrderBy(Expression<Func<T, object>>? orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+    protected void AddOrderByDescending(Expression<Func<T, object>>? orderByDescExpression)
+    {
+        OrderByDescending = orderByDescExpression;
+    }
+}
+
+public class BaseSpecification<T, TResult>(Expression<Func<T, bool>>? criteria) : BaseSpecifications<T>(criteria), ISpecification<T, TResult>
+{
+    public Expression<Func<T, TResult>>? Select
+    {
+        get; private set;
+    }
+
+    protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
+    {
+        Select = selectExpression;
+    }
+}
