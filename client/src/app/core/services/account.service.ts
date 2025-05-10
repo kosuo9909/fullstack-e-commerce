390785cb4469008e215acad184631c6a035ce771
kosuo9909 <stanevbsn@gmail.com>
2025-05-10 22:35:41 +0300

    add account service to client

---
+++ client/src/app/core/services/account.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { Address, User } from '@edi/app/shared/models/user';
+import { environment } from '@edi/environments/environment';
+import { map, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  public baseUrl = environment.apiUrl;
+  public currentUser = signal<User | null>(null);
+
+  private _http = inject(HttpClient);
+
+  public login(values: any) {
+    let params = new HttpParams();
+    params = params.append('useCookies', 'true');
+
+    return this._http
+      .post<User>(`${this.baseUrl}/login`, values, { params })
+      .pipe(map((user) => this.currentUser.set(user)));
+  }
+
+  public register(values: any) {
+    return this._http.post(`${this.baseUrl}/register`, values);
+  }
+
+  public getUserInfo() {
+    return this._http
+      .get<User>(`${this.baseUrl}/account/user-info`)
+      .pipe(tap((user) => this.currentUser.set(user)));
+  }
+
+  public logout() {
+    return this._http
+      .post(`${this.baseUrl}/logout`, {})
+      .pipe(tap(() => this.currentUser.set(null)));
+  }
+
+  public updateAddress(address: Address) {
+    return this._http.put(`${this.baseUrl}/account/address`, address);
+  }
+}
